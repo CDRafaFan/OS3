a59340e7eb6d04eeb64d56b59452a38b9d7eb703
CDRafaFan <113998036+CDRafaFan@users.noreply.github.com>
2022-11-24 12:41:50 +0530

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -23,12 +23,17 @@ int main()
         struct sched_param var;
         var.sched_priority=0;
         sched_setscheduler(0,SCHED_OTHER,&var);
-        execlve();
+        execl("/bin/bash","sh","process.sh",NULL);
     }
     else
     {
         wait(NULL);
         clock_gettime(CLOCK_REALTIME, &two);
+        double timea;
+        timea=(two.tv_sec-one.tv_sec)*billion;
+        timea+=(two.tv_nsec-one.tv_nsec);
+        timea/=billion;
+        printf("Time taken by process A %lf",timea);
         struct timespec pop1,pop2;
         clock_gettime(CLOCK_REALTIME, &pop1);
         int again=fork();
@@ -42,12 +47,17 @@ int main()
             struct sched_param var2;
             var2.sched_priority=10;
             sched_setscheduler(0,SCHED_RR,&var2);
-            execlve();
+            execl("/bin/bash","sh","process.sh",NULL);
         }
         else
         {
             wait(NULL);
             clock_gettime(CLOCK_REALTIME, &pop2);
+            double timeb;
+            timeb=(pop2.tv_sec-pop1.tv_sec)*billion;
+            timeb+=(pop2.tv_nsec-pop1.tv_nsec);
+            timeb/=billion;
+            printf("Time taken by process B %lf",timeb);
             struct timespec gov,har;
             clock_gettime(CLOCK_REALTIME, &gov);
             int last=fork();
@@ -61,27 +71,19 @@ int main()
                 struct sched_param var3;
                 var3.sched_priority=20;
                 sched_setscheduler(0,SCHED_FIFO,&var3);
-                execlve();
+                execl("/bin/bash","sh","process.sh",NULL);
             }
             else
             {
                 wait(NULL);
                 clock_gettime(CLOCK_REALTIME, &har);
+                double timec;
+                timec=(har.tv_sec-gov.tv_sec)*billion;
+                timec+=(har.tv_nsec-gov.tv_nsec);
+                timec/=billion;
+                printf("Time taken by process C %lf",timec);
             }
         }
     }
-    double timea,timeb,timec;
-    timea=(two.tv_sec-one.tv_sec)*billion;
-    timea+=(two.tv_nsec-one.tv_nsec);
-    timea/=billion;
-    timeb=(pop2.tv_sec-pop1.tv_sec)*billion;
-    timeb+=(pop2.tv_nsec-pop1.tv_nsec);
-    timeb/=billion;
-    timec=(har.tv_sec-gov.tv_sec)*billion;
-    timec+=(har.tv_nsec-gov.tv_nsec);
-    timec/=billion;
-    printf("Time taken by process A %lf",timea);
-    printf("Time taken by process A %lf",timeb);
-    printf("Time taken by process A %lf",timec);
     return 0;
 }
